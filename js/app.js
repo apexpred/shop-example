bfb20ea8a8e2d2b4963f1a3602e76ce0660ce647
David Omar <da.virus.crack@gmail.com>
2016-01-17 03:21:09 -0500

    added product img, title and price through from array of products instead of hard coded HTML

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+	var productsIMG = $('.thumbnail img');
+	var productsCaptions = $('.thumbnail .caption');
+
+	//adds the title, price and image for each product
+	function genProductsHTML(numProducts) {
+
+		for (var i = 0; i < numProducts; i++) {
+			var title = productsCaptions[i].children[1].children[0];
+			var price = productsCaptions[i].children[0];
+			productsIMG[i].setAttribute('src', 'img/' + products[i].category + '/' + products[i].name + '.png');
+			title.text = products[i].title;
+			price.innerHTML = '$' + parseInt(products[i].price.dollar + '.' + products[i].price.cent).toFixed(2);
+			
+		}
+
+	}
+
+	genProductsHTML(products.length);
+
+});
--- js/products.js
+++ js/products.js
@@ -1,1 +1,130 @@
-products.js
+var products = [
+	{
+		name: 'shirt-baylife',
+		title: 'Baylife T-shirt',
+		price: {
+			dollar: 19,
+			cent: 99
+		},
+		category: 'shirts'
+	},
+	{
+		name: 'shirt-black',
+		title: 'TSM Black',
+		price: {
+			dollar: 25,
+			cent: 0
+		},
+		category: 'shirts'
+	},
+	{
+		name: 'shirt-s4',
+		title: 'TSM Season 4',
+		price: {
+			dollar: 25,
+			cent: 0
+		},
+		category: 'shirts'
+	},
+	{
+		name: 'shirt-senpai',
+		title: 'Senpai T-shirt',
+		price: {
+			dollar: 19,
+			cent: 99
+		},
+		category: 'shirts'
+	},
+	{
+		name: 'shirt-white',
+		title: 'TSM White',
+		price: {
+			dollar: 25,
+			cent: 0
+		},
+		category: 'shirts'
+	},
+	{
+		name: 'jacket-s4',
+		title: 'Season 4 Jacket',
+		price: {
+			dollar: 60,
+			cent: 0
+		},
+		category: 'jackets'
+	},
+	{
+		name: 'hoody-player',
+		title: 'Player Name Hoody',
+		price: {
+			dollar: 59,
+			cent: 99
+		},
+		category: 'hoodies'
+	},
+	{
+		name: 'hoody-sponsor',
+		title: 'Sponsor Logo Hoody',
+		price: {
+			dollar: 55,
+			cent: 0
+		},
+		category: 'hoodies'
+	},
+	{
+		name: 'hat-tsm',
+		title: 'TSM Hat',
+		price: {
+			dollar: 32,
+			cent: 0
+		},
+		category: 'hats'
+	},
+	{
+		name: 'keychain-tsm',
+		title: 'TSM Keychain',
+		price: {
+			dollar: 4,
+			cent: 99
+		},
+		category: 'accessories'
+	},
+	{
+		name: 'mousepad-tsm',
+		title: 'TSM Mousepad',
+		price: {
+			dollar: 15,
+			cent: 0
+		},
+		category: 'accessories'
+	},
+	{
+		name: 'sticker-bjergsen',
+		title: 'Bjergsen Sticker',
+		price: {
+			dollar: 3,
+			cent: 0
+		},
+		category: 'accessories'
+	},
+	{
+		name: 'sticker-dyrus',
+		title: 'Dyrus Sticker',
+		price: {
+			dollar: 3,
+			cent: 0
+		},
+		category: 'accessories'
+	},
+	{
+		name: 'sticker-turtle',
+		title: 'Wildturtle Sticker',
+		price: {
+			dollar: 3,
+			cent: 0
+		},
+		category: 'accessories'
+	}
+
+];
+	
